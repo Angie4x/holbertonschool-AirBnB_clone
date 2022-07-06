183a96b6d95b8d8add72b304a980deb23da146d2
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-07-06 13:03:34 -0500

    pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,6 +15,7 @@ from models.user import User
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
+
 class FileStorage:
     """serializes instances to a JSON file & deserializes back to instances"""
 
@@ -27,13 +28,13 @@ class FileStorage:
         """returns the dictionary __objects"""
         return self.__objects
 
-def new(self, obj):
+    def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
-def save(self):
+    def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
@@ -41,14 +42,12 @@ def save(self):
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
-def reload(self):
+    def reload(self):
         """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
-        
-                               
