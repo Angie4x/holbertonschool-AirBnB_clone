5146a67f2e1ed169ed958db499604f80b4ccd836
Angie4x <angiel.aldanac@outlook.com>
2022-07-06 23:15:45 -0500

    ok

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,54 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-from datetime import datetime
-from models import storage  
-import uuid
+""" Base Model for the HBnB program.
+    """
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
+from datetime import datetime
+from models import storage
+import uuid
 
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+    """ Base Model for all classes used on HBnB.
+        """
 
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """ Initializes the BaseModel as base-class for the program.
+            """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
-            models.storage.save()
+            if "__class__" in kwargs.keys():
+                del kwargs["__class__"]
+            if "created_at" in kwargs.keys():
+                c_new_value = datetime.fromisoformat(kwargs["created_at"])
+                kwargs["created_at"] = c_new_value
+            if "updated_at" in kwargs.keys():
+                u_new_value = datetime.fromisoformat(kwargs["updated_at"])
+                kwargs["updated_at"] = u_new_value
+            self.__dict__.update(**kwargs)
+        storage.new(self)
 
     def __str__(self):
-        """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+        """ Prints an instances representation.
+            """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
+        """ Updates the instance updated_at attribute.
+            """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        return new_dict
+        """ Returns a dict() with all keys/values of instance.__dict__
+            """
+        dictionary = dict()
+        for (key, value) in (self.__dict__).items():
+            if isinstance(value, datetime):
+                dictionary[key] = value.isoformat()
+            else:
+                dictionary[key] = value
+        dictionary["__class__"] = self.__class__.__name__
+        return (dictionary)
